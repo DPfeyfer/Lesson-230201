63f739d3f1f75d06dcedcfea672d1369da8402bc
DPfeyfer <Skydenfer737@gmail.com>
2023-02-09 19:50:24 +0300

    equals hashcode correction

--- src/HW_230201/Homework230201.java
+++ src/HW_230201/Homework230201.java
@@ -5,12 +5,14 @@ public class Homework230201 {
         AuthorName astrid = new AuthorName("Astrid", "Lindgren");
         AuthorName lyman = new AuthorName("Lyman", "Frank");
         BookName karlsson = new BookName("Karlsson", astrid, 1955);
+        BookName karlsson1 = new BookName("Karlsson", astrid, 1955);
         BookName wizard = new BookName("The Wizard of Oz", lyman, 1939);
 
-        karlsson.setYearBook(1965);
+//        karlsson.setYearBook(1965);
         wizard.setYearBook(1939);
 
         System.out.println(karlsson);
         System.out.println(wizard);
+        System.out.println(karlsson.equals(karlsson1));
     }
 }
